d50cdd59a7813c5cb3eff1ab3ca15ca162c6ba70
Gleb <gkebkasyak@gmail.com>
2020-05-04 16:50:45 +0300

    refactor

--- client/src/components/Todo/TodoList.tsx
+++ client/src/components/Todo/TodoList.tsx
@@ -3,13 +3,13 @@ import { TodoType } from "../../shared/todoTypes";
 
 import TodoItem from "./TodoItem/TodoItem";
 
-type PropsType = {
+type Props = {
   todos: Array<TodoType>,
   markComplete: (id: number) => void,
   deleteTodo: (id: number) => void
 }
 
-export default ({ todos, markComplete, deleteTodo }: PropsType) => (
+export default ({ todos, markComplete, deleteTodo }: Props) => (
    <>
      { !!todos.length ? todos.map(todo =>
              <TodoItem
