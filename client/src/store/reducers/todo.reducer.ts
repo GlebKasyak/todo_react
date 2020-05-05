a293a10ee3cca0495653deb763c0a372d912d6d5
Gleb <gkebkasyak@gmail.com>
2020-05-05 11:10:21 +0300

    refactor reducer todo, add new ActionsType

--- client/src/store/reducers/todo.reducer.ts
+++ client/src/store/reducers/todo.reducer.ts
@@ -2,17 +2,14 @@ import { Reducer } from "redux";
 
 import * as todoTypes from "../types/todoTypes";
 import { TodoState } from "../../shared/todoTypes";
-import { InferActionsTypes } from "./index";
-import { todoActions } from "../actions/todo.action";
-
+import { ActionTypes } from "../actions/todo.action";
 
 const initialState: TodoState = {
     todos: []
 };
 
-type ActionsTypes = InferActionsTypes<typeof todoActions>;
 
-const reducer: Reducer<TodoState, ActionsTypes> = (state = initialState, action ) => {
+const reducer: Reducer<TodoState, ActionTypes> = (state = initialState, action ) => {
     switch (action.type) {
         case todoTypes.GET_ALL_TODOS:
             return { todos: action.payload };
