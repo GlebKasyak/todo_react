92fbed5e2272dbc5a9f310bc2a256ccb00981ba8
Gleb <gkebkasyak@gmail.com>
2020-05-05 11:09:39 +0300

    remove previous action type

--- client/src/store/reducers/index.ts
+++ client/src/store/reducers/index.ts
@@ -9,7 +9,5 @@ const rootReducer = combineReducers({
 type RootReducerType = typeof rootReducer;
 export type AppStateType = ReturnType<RootReducerType>
 
-type PropertiesTypes<T> = T extends { [key: string]: infer U } ? U : never;
-export type InferActionsTypes<T extends { [key: string]: (...args: any[]) => any } > = ReturnType<PropertiesTypes<T>>
 
 export default rootReducer;
